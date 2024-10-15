db06eef0305fd504b45a2422ed0e9108f25a6f98
Ivan-Eliseev <mrkatyt@gmail.com>
2024-10-15 22:19:14 +0300

    HW1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import  HW1  from './s2-homeworks/hw01/HW1';
+
+function App() {
+  return (
+    <div className="App">
+      <HW1 />
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
+    </div>
+  );
+}
+
+export default App;
