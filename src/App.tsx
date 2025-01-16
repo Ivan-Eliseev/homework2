a09261bf318680c5d58412409c8648197e0104cf
Ivan-Eliseev <mrkatyt@gmail.com>
2025-01-16 19:05:12 +0300

    Deploy to GitHub Pages

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+import HW4 from "./s2-homeworks/hw04/HW4";
+import HW1 from "./s2-homeworks/hw01/HW1";
+import HW2 from "./s2-homeworks/hw02/HW2";
+import HW3 from "./s2-homeworks/hw03/HW3";
+
+function App() {
+  return (
+    <div className="App">
+          {/*<HW1 />*/}
+          {/*<HW2 />*/}
+          {/*<HW3 />*/}
+          {/*<HW4 />*/}
+
+    </div>
+  );
+}
+
+export default App;
