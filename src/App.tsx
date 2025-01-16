ab218f1356d9fd6a2bd2b5e8f885e10800e0616c
Ivan-Eliseev <mrkatyt@gmail.com>
2025-01-16 18:39:12 +0300

    ервый коммит моего крутого проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
-import  HW1  from './s2-homeworks/hw01/HW1';
+
+import HW4 from "./s2-homeworks/hw04/HW4";
+import HW1 from "./s2-homeworks/hw01/HW1";
+import HW2 from "./s2-homeworks/hw02/HW2";
+import HW3 from "./s2-homeworks/hw03/HW3";
 
 function App() {
   return (
     <div className="App">
-      <HW1 />
-      {/*<HW2 />*/}
-      {/*<HW3 />*/}
-      {/*<HW4 />*/}
+          {/*<HW1 />*/}
+          {/*<HW2 />*/}
+          {/*<HW3 />*/}
+          {/*<HW4 />*/}
+
     </div>
   );
 }
